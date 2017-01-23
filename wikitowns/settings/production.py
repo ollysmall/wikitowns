19aa5841e31092a7fa24e95a86b0fbc8818ababe
Oliver Small <olly_small@hotmail.com>
2017-01-23 08:54:44 +0000

    additional files and changes for Heroku

--- wikitowns/settings/production.py
+++ wikitowns/settings/production.py
@@ -1,1 +1,9 @@
 from .base import *
+
+#settings from heroku tutorial
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*'] #this should change?
--- wikitowns/wsgi.py
+++ wikitowns/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wikitowns.settings")
 
 application = get_wsgi_application()
+
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
