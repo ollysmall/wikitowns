b070e8b0c988aa9a6e4ccae0473498c4eaf9b617
Oliver Small <olly_small@hotmail.com>
2017-01-23 10:36:04 +0000

    changed setting location in wsgi

--- wikitowns/wsgi.py
+++ wikitowns/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wikitowns.settings")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wikitowns.settings")
 
 application = get_wsgi_application()
 
