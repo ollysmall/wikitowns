a5aa9b7fd446a6c7a6430f4d331255c888af3fff
Oliver Small <olly_small@hotmail.com>
2017-02-09 19:02:12 +0000

    turned off debug

--- wikitowns/settings/base.py
+++ wikitowns/settings/base.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ollysmall.pythonanywhere.com', '127.0.0.1'] #change if production
 
