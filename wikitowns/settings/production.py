2ad7215271bb47cebc852672b1803d68ed940de9
Oliver Small <olly_small@hotmail.com>
2018-01-11 15:24:31 +0000

    removed whitenoise from production settings

--- wikitowns/settings/production.py
+++ wikitowns/settings/production.py
@@ -1,5 +1,5 @@
 from .base import *
-from whitenoise import WhiteNoise
+#from whitenoise import WhiteNoise
 
 # settings from heroku tutorial
 import dj_database_url
@@ -20,4 +20,4 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
